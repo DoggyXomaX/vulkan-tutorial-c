7b2181be8058a38acc9a43ff8fdcef79d27bef6d
DoggyXomaX <l_l_l_l-l_l_l_l@mail.ru>
2022-02-16 22:58:58 +0300

    Fixed: enum value of storeOp changed to VK_ATTACHMENT_LOAD_OP_DONT_CARE

--- src/HelloTriangleApplication.c
+++ src/HelloTriangleApplication.c
@@ -686,7 +686,7 @@ VkResult CreateRenderPass() {
         .loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
         .storeOp = VK_ATTACHMENT_STORE_OP_STORE,
         .stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
-        .stencilStoreOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+        .stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
         .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
         .finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
     };
